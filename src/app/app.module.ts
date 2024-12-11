43f144ad1dac6042601bca7764d15b06c1ef8dc1
Reshma <reshmakolekar72@gmail.com>
2024-12-11 17:41:05 +0530

    New Comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MotorCompComponent } from './motor-comp/motor-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MotorCompComponent
   ],
   imports: [
     BrowserModule
